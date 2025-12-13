d53bb6fb06a06b2ccb68a267b3d597700ce054a6
AVJoshua <akalihujoshua@gmail.com>
2025-12-13 22:53:52 +0100

    Got Basic Stock Info

--- stockAnalyze.py
+++ stockAnalyze.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+from datetime import datetime
 
 def extractBasicInfo(data):
     keysToExtract = ['longName', 'website','sector', 'fullTimeEmployees', 'marketCap', 'totalRevenue', 'trailingEps']
@@ -7,12 +8,46 @@ def extractBasicInfo(data):
         if key in data:
             basicInfo[key] = data[key]
         else:
-            basicInfo[key] = ""
+            basicInfo[key] = ''
     return basicInfo
 
-def getCompanyStockInfo(tickerSymbol):
-    company = yf.Ticker(tickerSymbol)
-    basicInfo = extractBasicInfo(company.info)
-    print(basicInfo)
+def getPriceHistory(company):
+    historyDf = company.history(period='12mo')
+    prices = historyDf['Open'].tolist()
+    dates = historyDf.index.strftime('%Y-%m-%d').tolist()
+    return {
+        'price': prices,
+        'date': dates
+    }
 
-getCompanyStockInfo("MSFT")
+def getEarningsDates(company):
+    earningsDatesDf = company.earnings_dates
+    allDates = earningsDatesDf.index.strftime('%Y-%m-%d').tolist()
+    dateObjects = [datetime.strptime(date, '%Y-%m-%d') for date in allDates]
+    currentDate = datetime.now()
+    futureDates = [date.strftime('%Y-%m-%d') for date in dateObjects if date > currentDate]
+    return futureDates
+
+def getCompanyNews(company):
+    newsList = company.news
+    allNewsArticle = []
+    for newsDict in newsList:
+        newsDictToAdd = {
+            'title': newsDict['content']['title'],
+            'link': newsDict['content']['thumbnail']
+        }
+        allNewsArticle.append(newsDictToAdd)
+    return allNewsArticle
+
+def getCompanyStockInfo(tickerSymbol):
+    #Get data from Yahoo Finance API
+    company = yf.Ticker(tickerSymbol)
+
+    # Get basic info on company
+    basicInfo = extractBasicInfo(company.info)
+    priceHistory = getPriceHistory(company)
+    futureEarningsDate = getEarningsDates(company)
+    newsArticle = getCompanyNews(company)
+    print(priceHistory)
+
+getCompanyStockInfo('MSFT')
