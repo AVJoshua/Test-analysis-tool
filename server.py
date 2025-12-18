b2a80d29de9d6dbc8e2aef20d4c57f8e478eaa3a
AVJoshua <akalihujoshua@gmail.com>
2025-12-18 01:56:54 +0100

    Added analyze-stock request handler

--- server.py
+++ server.py
@@ -9,10 +9,14 @@ app = Flask(__name__)
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
+@app.route('/health')
+def healthCheck():
+    return 'Flask server is up and running'
+
+@app.route('/analyze-stock')
+def analyzeStock():
+    return {'data': 'Analysis coming soon'}
+
 
 # main driver function
 if __name__ == '__main__':
