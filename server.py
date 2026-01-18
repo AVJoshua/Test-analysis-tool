d1347dcc40ff31d7e41e974e0fb66e30962f40c3
AVJoshua <akalihujoshua@gmail.com>
2026-01-18 17:35:39 +0100

    Updated Cors origin for josval.xyz

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ stockDataTest = json.load(f)
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
-CORS(app, origins=["https://avjoshua.github.io"])
+CORS(app, origins=["https://avjoshua.github.io", "https://www.josval.xyz"])
 
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
