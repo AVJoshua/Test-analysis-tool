eb860f7c8f86b06c5288e0584e577dfd93ca6d74
AVJoshua <akalihujoshua@gmail.com>
2026-04-06 16:55:42 +0100

    Add Procfile and configure Flask app to use environment variable for port

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 import json
+import os
 from flask import Flask, abort, request
 from flask_cors import CORS
 from stockAnalyze import getCompanyStockInfo
@@ -47,4 +48,5 @@ def analyzeTextHandler():
 if __name__ == '__main__':
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(host="0.0.0.0")
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
